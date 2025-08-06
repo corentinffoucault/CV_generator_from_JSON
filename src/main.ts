e6ac0f7bf018d15e134cf4751f7505b6fc6501b4
corentin <corentinf@free.fr>
2025-08-06 14:32:06 +0200

    full operationnel project

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-import Resume from "../packages/cv-template/src/resume.js";
+import { Command } from 'commander';
+
+import ResumeGenerator from "../../libs/typescript_cv_template/src/ResumeConvertor.js";
+import { promises as fs } from 'fs';
+import { homedir } from 'os';
+import * as path from 'path';
 
 async function generate() {
-    console.log(new Resume().print());
+    const program = new Command();
+
+    program.command('generate')
+        .description('generate html resume from json')
+        .option('--output <string>', 'path to html resume output')
+        .option('--lang <string>', 'language of the resume')
+        .argument('<string>', 'path to json resume input')
+        .action(async (jsonPath, options) => {
+            const resume = await fs.readFile(path.join(homedir(), jsonPath));
+            const resumeHtml = await (new ResumeGenerator()).generateResume(JSON.parse(resume.toString()));
+            fs.writeFile(path.join(homedir(), options.output), resumeHtml);
+        });
+    program.parse();
 }
 
 void generate();
