c23db770d0a83e650d97da4cdb4e44cc9fe7e6ea
corentin <corentinf@free.fr>
2025-08-25 16:02:50 +0200

    ajout du controler

--- src/commander/Commander.ts
+++ src/commander/Commander.ts
@@ -1,7 +1,7 @@
 import { Command } from 'commander';
 
-import { IResumeConvertor } from "../../../libs/json_cv_schema/src/type/IResumeConvertor.js";
-import { promises as fs } from 'fs';
+import { Controler, IResumeConvertor } from "../../packages/json_cv_schema/src/Index.js";
+import * as fs from 'node:fs/promises';
 import { homedir } from 'os';
 import * as path from 'path';
 
@@ -12,12 +12,13 @@ export class Commander {
         const program = new Command();
         program.command('generate')
             .description('generate html resume from json')
-            .option('--output <string>', 'path to html resume output')
-            .option('--lang <string>', 'language of the resume')
+            .requiredOption('--output <string>', 'path to html resume output')
+            .requiredOption('--lang <string>', 'language of the resume')
             .argument('<string>', 'path to json resume input')
             .action(async (jsonPath, options) => {
-                const resume = await fs.readFile(path.join(homedir(), jsonPath));
-                const resumeHtml = await this.resumeConvertor.generateResume(JSON.parse(resume.toString()));
+                const resume = JSON.parse((await fs.readFile(path.join(homedir(), jsonPath))).toString());
+                Controler.isValidResume(resume);
+                const resumeHtml = await this.resumeConvertor.generateResume(resume);
                 fs.writeFile(path.join(homedir(), options.output), resumeHtml);
             });
         program.parse();
